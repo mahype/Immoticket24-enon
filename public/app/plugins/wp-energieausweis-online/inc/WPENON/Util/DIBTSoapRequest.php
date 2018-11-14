558fa2426c02ed23853723f2c4d4d338c25b1bd2
Sven Wagener <sven@awesome.ug>
2018-11-14 12:26:17 +0100

    Code autoformat

--- public/app/plugins/wp-energieausweis-online/inc/WPENON/Util/DIBTSoapRequest.php
+++ public/app/plugins/wp-energieausweis-online/inc/WPENON/Util/DIBTSoapRequest.php
@@ -8,97 +8,90 @@
 namespace WPENON\Util;
 
 class DIBTSoapRequest {
-  protected $api_url = '';
-  
-  protected $action = '';
-  protected $args = array();
-  
-  protected $response = null;
-  
-  public function __construct( $action, $args = array() ) {
-    $slug = 'published';
-    if ( WPENON_DEBUG ) {
-      $slug = 'sandbox';
-    }
-    $this->api_url = 'https://energieausweis.dibt.de/' . $slug . '/WebServiceEnergieausweis/DibtEnergieAusweisService.asmx';
+	protected $api_url = '';
 
-    $actions = $this->getValidActions();
-    if ( isset( $actions[ $action ] ) ) {
-      $this->action = $action;
-    } else {
-      new \WPENON\Util\Error( 'fatal', __METHOD__, sprintf( __( 'Die Aktion %s ist keine gültige DIBT-API-Funktion.', 'wpenon' ), $action ), '1.0.0' );
-    }
-    
-    foreach ( $actions[ $this->action ] as $key => $default ) {
-      $this->args[ $key ] = isset( $args[ $key ] ) ? $args[ $key ] : $default;
-    }
-  }
-  
-  public function send() {
-    $response = null;
+	protected $action = '';
+	protected $args = array();
 
-    $function = $this->action;
-    $response_type = $this->action . 'Result';
+	protected $response = null;
 
-    $xml = '<' . $function . ' xmlns="http://energieausweis.dibt.de/WebServiceEnergie/DibtEnergieAusweisService">';
-    foreach ( $this->args as $key => $value ) {
-      $xml .= '<' . $key . '>' . $value . '</' . $key . '>';
-    }
-    $xml .= '</' . $function . '>';
+	public function __construct( $action, $args = array() ) {
+		$slug = 'published';
+		if ( WPENON_DEBUG ) {
+			$slug = 'sandbox';
+		}
+		$this->api_url = 'https://energieausweis.dibt.de/' . $slug . '/WebServiceEnergieausweis/DibtEnergieAusweisService.asmx';
 
-    $soap = new \SoapClient( $this->api_url . '?WSDL', array(
-      'cache_wsdl'    => WSDL_CACHE_NONE,
-      'trace'         => 1,
-    ) );
+		$actions = $this->getValidActions();
+		if ( isset( $actions[ $action ] ) ) {
+			$this->action = $action;
+		} else {
+			new \WPENON\Util\Error( 'fatal', __METHOD__, sprintf( __( 'Die Aktion %s ist keine gültige DIBT-API-Funktion.', 'wpenon' ), $action ), '1.0.0' );
+		}
 
-    $request_body = new \SoapVar( $xml, XSD_ANYXML );
+		foreach ( $actions[ $this->action ] as $key => $default ) {
+			$this->args[ $key ] = isset( $args[ $key ] ) ? $args[ $key ] : $default;
+		}
+	}
 
-    try {
-      $response = $soap->$function( $request_body );
-    } catch( \SoapFault $exception ) {
-      new \WPENON\Util\Error( 'notice', __METHOD__, sprintf( __( 'DIBT Soap Fehler: %s', 'wpenon' ), $exception->getMessage() ), '1.0.0' );
-    }
+	public function send() {
+		$response = null;
 
-    if ( ! is_soap_fault( $response ) ) {
-      if ( isset( $response->$response_type ) ) {
-        $response = $response->$response_type;
-      }
-      if ( isset( $response->any ) ) {
-        $response = $response->any;
-      }
-      $this->response = $response;
-    }
-  }
-  
-  public function getAction() {
-    return $this->action;
-  }
-  
-  public function getArgs() {
-    return $this->args;
-  }
-  
-  public function getResponse() {
-    return $this->response;
-  }
-  
-  protected function getValidActions() {
-    $actions = array(
-      'Datenregistratur'      => array(
-        'doc'                   => '',
-      ),
-      'ZusatzdatenErfassung'  => array(
-        'doc'                   => '',
-        'ausweisnummer'         => '',
-        'username'              => '',
-        'passwort'              => '',
-      ),
-      'Restkontingent'        => array(
-        'Username'              => '',
-        'Passwort'              => '',
-      ),
-    );
+		$function      = $this->action;
+		$response_type = $this->action . 'Result';
 
-    return $actions;
-  }
+		$xml = '<' . $function . ' xmlns="http://energieausweis.dibt.de/WebServiceEnergie/DibtEnergieAusweisService">';
+		foreach ( $this->args as $key => $value ) {
+			$xml .= '<' . $key . '>' . $value . '</' . $key . '>';
+		}
+		$xml .= '</' . $function . '>';
+
+		try {
+			$response = $soap->$function( $request_body );
+		} catch ( \SoapFault $exception ) {
+			new \WPENON\Util\Error( 'notice', __METHOD__, sprintf( __( 'DIBT Soap Fehler: %s', 'wpenon' ), $exception->getMessage() ), '1.0.0' );
+		}
+
+		if ( ! is_soap_fault( $response ) ) {
+			if ( isset( $response->$response_type ) ) {
+				$response = $response->$response_type;
+			}
+			if ( isset( $response->any ) ) {
+				$response = $response->any;
+			}
+			$this->response = $response;
+		}
+	}
+
+	public function getAction() {
+		return $this->action;
+	}
+
+	public function getArgs() {
+		return $this->args;
+	}
+
+	public function getResponse() {
+		return $this->response;
+	}
+
+	protected function getValidActions() {
+		$actions = array(
+			'Datenregistratur'     => array(
+				'doc' => '',
+			),
+			'ZusatzdatenErfassung' => array(
+				'doc'           => '',
+				'ausweisnummer' => '',
+				'username'      => '',
+				'passwort'      => '',
+			),
+			'Restkontingent'       => array(
+				'Username' => '',
+				'Passwort' => '',
+			),
+		);
+
+		return $actions;
+	}
 }
