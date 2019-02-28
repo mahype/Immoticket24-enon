1c6edb3e618a69d7b57b761e7ea1a3a4a16caa3d
Sven Wagener <sven@awesome.ug>
2019-02-28 13:05:36 +0100

    Commented out extended logs for sofort.

--- public/app/plugins/wp-energieausweis-online/inc/WPENON/Model/PaymentGatewaySofortueberweisung.php
+++ public/app/plugins/wp-energieausweis-online/inc/WPENON/Model/PaymentGatewaySofortueberweisung.php
@@ -64,7 +64,7 @@ class PaymentGatewaySofortueberweisung extends \WPENON\Model\PaymentGateway {
 
 		$sofortueberweisung->sendRequest();
 
-		$this->log( sprintf( 'Doing Request with purchase data: %s  Payment data: %s Sofort Object: %s', var_export( $purchase_data, true ), var_export( $payment_data, true ) , var_export( $sofortueberweisung, true ) ) );
+		// $this->log( sprintf( 'Doing Request with purchase data: %s  Payment data: %s Sofort Object: %s', var_export( $purchase_data, true ), var_export( $payment_data, true ) , var_export( $sofortueberweisung, true ) ) );
 
 		if ( $sofortueberweisung->isError() ) {
 			$this->_handlePaymentError( $payment_id, sprintf( __( 'Sofortueberweisung %1$s. Request data: %2$s', 'wpenon' ), $sofortueberweisung->getError(), json_encode( $sofortueberweisung->getData() ) ), true );
@@ -116,7 +116,7 @@ class PaymentGatewaySofortueberweisung extends \WPENON\Model\PaymentGateway {
 			$this->_handlePaymentProcessError( null, __( 'Missing POST data.', 'wpenon' ), true );
 		}
 
-		$this->log( sprintf( 'Incoming process purchase notification: %s', var_export( $input, true ) ) );
+		// $this->log( sprintf( 'Incoming process purchase notification: %s', var_export( $input, true ) ) );
 
 		$notification = new \Sofort\SofortLib\Notification();
 
