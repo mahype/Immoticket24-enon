d21dd608d6b80ce251c9746cabb7f8d23d8aaf71
Sven Wagener <sven@awesome.ug>
2019-07-18 12:27:15 +0200

    Changed from wrong hook for setting sender.

--- public/app/themes/immoticket-energieausweis/inc/whitelabel-confirmation-email.php
+++ public/app/themes/immoticket-energieausweis/inc/whitelabel-confirmation-email.php
@@ -10,8 +10,8 @@ class EA_Whitelabel_Confirmation_Email extends EA_Whitelabel_Email {
 	public function init_hooks() {
 		parent::init_hooks();
 
-		add_filter( 'wpenon_order_confirmation_from_address', array( $this, 'set_from_address' ) );
-		add_filter( 'wpenon_order_confirmation_from_name', array( $this, 'set_from_name' ) );
+		add_filter( 'wpenon_confirmation_from_address', array( $this, 'set_from_address' ) );
+		add_filter( 'wpenon_confirmation_from_name', array( $this, 'set_from_name' ) );
 		add_filter( 'wpenon_confirmation_link', array( $this, 'set_link' ), 10, 2 );
 		add_filter( 'wpenon_confirmation_site', array( $this, 'set_site' ), 10, 1 );
 
@@ -28,7 +28,8 @@ class EA_Whitelabel_Confirmation_Email extends EA_Whitelabel_Email {
 	 * @return string Tokens from email address.
 	 */
 	public function set_from_address( $email = '' ) {
-		return $this->whitelabel->get_email_from_address();
+		$email_from_address = $this->whitelabel->get_email_from_address();
+		return $email_from_address;
 	}
 
 	/**
@@ -39,7 +40,8 @@ class EA_Whitelabel_Confirmation_Email extends EA_Whitelabel_Email {
 	 * @return string Tokens from email name.
 	 */
 	public function set_from_name( $email = '' ) {
-		return $this->whitelabel->get_email_from_name();
+		$from_name = $this->whitelabel->get_email_from_name();
+		return $from_name;
 	}
 
 	/**
