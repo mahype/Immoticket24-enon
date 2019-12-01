c758d549a4cce51d16f31c7fccc8a301ff8609ce
Sven Wagener <sven@awesome.ug>
2019-12-01 16:31:52 +0100

    Completed logic for  sending data.

--- public/app/plugins/enon/src/Whitelabel/WordPress/Enon/SendEnergieausweis.php
+++ public/app/plugins/enon/src/Whitelabel/WordPress/Enon/SendEnergieausweis.php
@@ -2,16 +2,18 @@
 
 namespace Enon\Whitelabel\WordPress\Enon;
 
+use Awsm\WPWrapper\BuildingPlans\Actions;
 use Enon\Logger;
 use Enon\Traits\Logger as LoggerTrait;
 use Enon\Whitelabel\Reseller;
+use WPENON\Model\Energieausweis;
 
 /**
  * Class SendEnergieausweis
  *
  * @since 1.0.0
  */
-class SendEnergieausweis {
+class SendEnergieausweis implements Actions {
 	use LoggerTrait;
 
 	/**
@@ -34,5 +36,63 @@ class SendEnergieausweis {
 	{
 		$this->reseller = $reseller;
 		$this->logger = $logger;
+
+		// No endpoint, no data.
+		if( empty( $this->reseller->data()->getPostEndpoint() ) ) {
+			return;
+		}
+
+		$this->addActions();
+	}
+
+	/**
+	 * Adding actions.
+	 *
+	 * @since 1.0.0
+	 */
+	public function addActions()
+	{
+		add_action( 'wpenon_payment_successful', [ $this, 'sendData' ] );
+	}
+
+	/**
+	 * Get energieausweis id by payment id.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $paymentId Payment id.
+	 * @return int Energieausweis id.
+	 *
+	 * @todo Move this to energieausweis section.
+	 */
+	private function getEnergieausweisIdByPaymentId( $paymentId )
+	{
+		$payment_meta = edd_get_payment_meta( $paymentId  );
+		$item = array_shift( $payment_meta['cart_details'] );
+		return $item[ 'id' ];
+	}
+
+	/**
+	 * Send data.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $paymentId Payment id.
+	 */
+	public function sendData( $paymentId )
+	{
+		$senderClassName = $this->reseller->data()->getPostDataConfigClass();
+		$senderClassName = 'Enon\\Whitelabel\\PostData\\' . $senderClassName;
+
+		if( ! class_exists( $senderClassName ) ) {
+			$this->logger()->warning( sprintf( 'Sender Class %s does not exist, Do not send data.', $senderClassName ) );
+			return;
+		}
+
+		$energieausweis = new Energieausweis( $this->getEnergieausweisIdByPaymentId( $paymentId) );
+
+		$senderEndpoint = $this->reseller->data()->getPostEndpoint();
+		$postEnergieausweis = new $senderClassName( $senderEndpoint, $energieausweis, $this->logger() );
+		$postEnergieausweis->send();
 	}
 }
