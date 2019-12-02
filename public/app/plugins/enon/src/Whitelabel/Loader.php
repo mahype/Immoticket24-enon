b07de16b7f07a861c28d53e3ae4f470e357d6cc2
Sven Wagener <sven@awesome.ug>
2019-12-02 13:52:11 +0100

    Splitted tasks in front- and backend

--- public/app/plugins/enon/src/Whitelabel/Loader.php
+++ public/app/plugins/enon/src/Whitelabel/Loader.php
@@ -2,6 +2,7 @@
 
 namespace Enon\Whitelabel;
 
+use Awsm\WPWrapper\BuildingPlans\Actions;
 use Enon\TaskLoader;
 use Enon\Exceptions\Exception;
 use Enon\Whitelabel\WordPress\Core\CPTReseller;
@@ -26,15 +27,41 @@ class Loader extends TaskLoader {
 	 * @since 1.0.0
 	 */
 	public function run() {
-		$token = new Token();
-
 		$this->addTask( CPTReseller::class );
 		$this->addTask( ACF::class, $this->logger() );
 
 		if( is_admin() ) {
-			$this->runTasks();;
-			return;
+			$this->runAdminTasks();
+		} else {
+		    $this->runFrontendTasks();
 		}
+	}
+
+	/**
+	 * Running admin tasks.
+	 *
+	 * @since 1.0.0
+	 */
+	public function runAdminTasks()
+	{
+		try {
+			$resellerData = new ResellerData();
+			$reseller = new Reseller( $resellerData,  $this->logger() );
+		} catch ( Exception $exception ) {
+			$this->logger()->error( sprintf( $exception->getMessage() ) );
+		}
+
+		$this->addTask( SendEnergieausweis::class, $reseller, $this->logger() );
+		$this->runTasks();
+	}
+
+	/**
+	 * Running frontend tasks.
+	 *
+	 * @since 1.0.0
+	 */
+	public function runFrontendTasks() {
+		$token = new Token();
 
 		// No token, no action
 		if( empty( $token->get() ) ) {
@@ -50,12 +77,11 @@ class Loader extends TaskLoader {
 
 		$this->addTask( Frontend::class );
 		$this->addTask( Enon::class, $reseller, $this->logger() );
-		$this->addTask( EmailConfirmation::class, $reseller, $this->logger() );
-		$this->addTask( EmailOrderConfirmation::class, $reseller, $this->logger() );
-		$this->addTask( SendEnergieausweis::class, $reseller, $this->logger() );
-		$this->addTask( AffiliateWP::class, $reseller, $this->logger() );
-		$this->addTask( ACF::class, $this->logger() );
-		$this->addTask( Edd::class, $reseller, $this->logger() );
+		// $this->addTask( EmailConfirmation::class, $reseller, $this->logger() );
+		// $this->addTask( EmailOrderConfirmation::class, $reseller, $this->logger() );
+		// $this->addTask( SendEnergieausweis::class, $reseller, $this->logger() );
+		// $this->addTask( AffiliateWP::class, $reseller, $this->logger() );
+		// $this->addTask( Edd::class, $reseller, $this->logger() );
 
 		$this->runTasks();;
 	}
