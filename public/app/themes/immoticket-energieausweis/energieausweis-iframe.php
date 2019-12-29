f6f9f9efb194151aa961fc5ad99e6ba54cfa3e84
Sven Wagener <sven@awesome.ug>
2019-12-29 12:54:30 +0100

    Formatted code

--- public/app/themes/immoticket-energieausweis/energieausweis-iframe.php
+++ public/app/themes/immoticket-energieausweis/energieausweis-iframe.php
@@ -7,50 +7,51 @@
 ?><!DOCTYPE html>
 <html <?php echo $html_attrs; ?> <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php immoticketenergieausweis_wp_title( '|' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<?php wp_head(); ?>
-<style type="text/css">
-  .energieausweis-iframe {
-    background-color: white;
-    background-image: none;
-  }
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php immoticketenergieausweis_wp_title( '|' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	<style type="text/css">
+		.energieausweis-iframe {
+			background-color: white;
+			background-image: none;
+		}
 
-  .energieausweis-iframe .wrapper {
-    padding: 0 30px;
-  }
+		.energieausweis-iframe .wrapper {
+			padding: 0 30px;
+		}
 
-  .energieausweis-iframe .primary.col-md-12 {
-    padding-right: 30px !important;
-    padding-left: 30px !important;
-  }
-</style>
+		.energieausweis-iframe .primary.col-md-12 {
+			padding-right: 30px !important;
+			padding-left: 30px !important;
+		}
+		<?php do_action('enon_iframe_css' ); ?>
+	</style>
 </head>
 
 <body <?php body_class( 'energieausweis-iframe' ); ?>>
 
-  <div class="wrapper">
-    <div class="content">
+<div class="wrapper">
+	<div class="content">
 
-      <div class="row">
+		<div class="row">
 
-        <main class="primary col-md-12" role="main">
-          <?php if( have_posts() ) : ?>
-            <?php while( have_posts() ) : the_post(); ?>
-              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-              </article>
-            <?php endwhile; ?>
-          <?php endif; ?>
-        </main>
+			<main class="primary col-md-12" role="main">
+				<?php if ( have_posts() ) : ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<h1><?php the_title(); ?></h1>
+							<?php the_content(); ?>
+						</article>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</main>
 
-      </div>
+		</div>
 
-    </div>
-  </div><!-- .wrapper -->
+	</div>
+</div><!-- .wrapper -->
 
 <?php wp_footer(); ?>
 
