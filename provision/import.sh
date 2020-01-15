286fbeb270cc3691bc05e90e280a44010a694d70
Sven Wagener <sven@awesome.ug>
2020-01-15 10:41:20 +0100

    Udpated import script

--- provision/import.sh
+++ provision/import.sh
@@ -4,13 +4,17 @@
 # Importing data from livesite
 ###############################
 
-FILE=db.sql
+IMPORT_FILE=db.sql
+DB_NAME=enon
+DB_USER=root
+DB_PASS=root
 
-if [ -f "$FILE" ]; then
-    mysql -u root --password=root enon < $FILE
-	wp search-replace "energieausweis-online-erstellen.de" "enon.test"
-	wp search-replace "www.energieausweis-online-erstellen.de" "enon.test"
-	wp search-replace "https://enon.test" "http://enon.test"
+ONLINE_DOMAIN=energieausweis-online-erstellen.de
+LOCAL_DOMAIN=enon.test
+
+if [ -f "$IMPORT_FILE" ]; then
+    mysql -u $DB_USER --password=$DB_PASS $DB_NAME < $IMPORT_FILE
+	wp search-replace $ONLINE_DOMAIN $LOCAL_DOMAIN
 	wp scrub posts --post_type=download --date='-3 month'
 	echo "Finished!"
 else
