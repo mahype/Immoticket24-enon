119875ee10b3970a273f3ba162853be19d3ffa70
Sven Wagener <sven@awesome.ug>
2020-03-12 15:36:51 +0100

    Removing modernizations which will be made afterwards - Issue #38

--- public/app/wpenon/enev2020-01/Modernizations.php
+++ public/app/wpenon/enev2020-01/Modernizations.php
@@ -113,4 +113,46 @@ class Modernizations {
 
 		return false;
 	}
+
+	/**
+	 * Removing modernizations.
+	 *
+	 * @param array $modernizations  Modernizations.
+	 * @param array $slugs_to_remove Modernization slugs to remove.
+	 *
+	 * @return array Filtered modernization slugs.
+	 *
+	 * @since 1.0.0
+	 */
+	public function remove_modernizations( array $modernizations, array $slugs_to_remove ) {
+		foreach ( $slugs_to_remove as $slug ) {
+			if ( ! array_key_exists( $slug, $modernizations ) ) {
+				continue;
+			}
+
+			unset( $modernizations[ $slug ] );
+		}
+
+		return $modernizations;
+	}
+
+	/**
+	 * Check if moderinzation is alreade added.
+	 *
+	 * @param array $modernizations     Modernizations.
+	 * @param array $modernization_info Single Modernization information.
+	 *
+	 * @return bool
+	 *
+	 * @since 1.0.0
+	 */
+	public function modernization_already_added( array $modernizations, array $modernization_info ) {
+		foreach ( $modernizations as $modernization ) {
+			if ( $modernization['bauteil'] === $modernization_info['bauteil'] ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
