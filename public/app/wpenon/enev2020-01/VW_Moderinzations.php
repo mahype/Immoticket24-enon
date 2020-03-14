28fb419c621cf7de298b5ee5e7dbce70e7165e80
Sven Wagener <sven@awesome.ug>
2020-03-14 16:17:41 +0100

    Added heaters elektrodirektheizgeraet & kohleholzofen to current schema

--- public/app/wpenon/enev2020-01/VW_Moderinzations.php
+++ public/app/wpenon/enev2020-01/VW_Moderinzations.php
@@ -42,30 +42,30 @@ class VW_Modernizations extends Modernizations {
 		$this->energieausweis = $energieausweis;
 
 		// Remove modernizations which are checked afterwards.
-		$slugs_to_remove = [ 'wand', 'decke', 'boden', 'dach', 'rohrleitungssystem', 'solarthermie' ];
+		$slugs_to_remove = [ 'wand', 'decke', 'boden', 'dach', 'rohrleitungssystem', 'solarthermie', 'heizung' ];
 		$modernizations  = $this->remove_modernizations( $modernizations, $slugs_to_remove );
 
 		// Checking for modernizations.
-		if ( 1995 > $energieausweis->baujahr && 'no' === $energieausweis->wand_daemmung_on ) {
+		if ( 1995 > $this->energieausweis->baujahr && 'no' === $this->energieausweis->wand_daemmung_on ) {
 			$modernization = $this->get_modernization( 'wand' );
 			if ( ! $this->modernization_already_added( $modernizations, $modernization ) ) {
 				$modernizations[] = $modernization;
 			}
 		}
 
-		if ( 1995 > $energieausweis->baujahr && 'no' === $energieausweis->decke_daemmung_on && ( 'nicht-vorhanden' === $energieausweis->dach || 'unbeheizt' === $energieausweis->dach ) ) {
+		if ( 1995 > $this->energieausweis->baujahr && 'no' === $this->energieausweis->decke_daemmung_on && ( 'nicht-vorhanden' === $this->energieausweis->dach || 'unbeheizt' === $this->energieausweis->dach ) ) {
 			$modernization = $this->get_modernization( 'decke' );
 			if ( ! $this->modernization_already_added( $modernizations, $modernization ) ) {
 				$modernizations[] = $modernization;
 			}
 		}
 
-		if ( 1995 > $energieausweis->baujahr && 'no' === $energieausweis->boden_daemmung_on && 'unbeheizt' === $energieausweis->keller ) {
+		if ( 1995 > $this->energieausweis->baujahr && 'no' === $this->energieausweis->boden_daemmung_on && 'unbeheizt' === $this->energieausweis->keller ) {
 			$modernization    = $this->get_modernization( 'boden' );
 			$modernizations[] = $modernization;
 		}
 
-		if ( 1995 > $energieausweis->baujahr && 'no' === $energieausweis->dach_daemmung_on && 'beheizt' === $energieausweis->dach ) {
+		if ( 1995 > $this->energieausweis->baujahr && 'no' === $this->energieausweis->dach_daemmung_on && 'beheizt' === $this->energieausweis->dach ) {
 			$modernization    = $this->get_modernization( 'dach' );
 			$modernizations[] = $modernization;
 		}
@@ -80,13 +80,18 @@ class VW_Modernizations extends Modernizations {
 			'oelofenverdampfungsbrenner',
 		);
 
-		if ( 1995 > $energieausweis->verteilung_baujahr && ! $energieausweis->verteilung_gedaemmt && ! in_array( $energieausweis->h_erzeugung, $irrelevant_heaters ) ) {
+		if ( 1995 > $this->energieausweis->verteilung_baujahr && ! $this->energieausweis->verteilung_gedaemmt && ! in_array( $this->energieausweis->h_erzeugung, $irrelevant_heaters ) ) {
 			$modernization    = $this->get_modernization( 'rohrleitungssystem' );
 			$modernizations[] = $modernization;
 		}
 
 		if ( $this->needs_solarthermie() ) {
 			$modernization    = $this->get_modernization( 'solarthermie' );
+			$modernizations[] = $modernization;
+		}
+
+		if ( $this->needs_heater() ) {
+			$modernization    = $this->get_modernization( 'heizung' );
 			$modernizations[] = $modernization;
 		}
 
@@ -131,5 +136,47 @@ class VW_Modernizations extends Modernizations {
 			default:
 				return true;
 		}
+	}
+
+	/**
+	 * Needs heater.
+	 *
+	 * @return bool
+	 *
+	 * @since 1.0.0
+	 */
+	protected function needs_heater() {
+		$heatings = array( 'h' );
+
+		$min_age = 30;
+
+		if ( isset( $this->energieausweis->h2_info ) && $this->energieausweis->h2_info ) {
+			$heatings[] = 'h2';
+		}
+
+		if ( isset( $this->energieausweis->h3_info ) && $this->energieausweis->h3_info ) {
+			$heatings[] = 'h3';
+		}
+
+		$current_year = absint( current_time( 'Y' ) );
+
+		$types       = array(
+			'gasraumheizer',
+			'elektrodirektheizgeraet',
+			'elektronachtspeicherheizung',
+			'oelofenverdampfungsbrenner',
+			'kohleholzofen',
+		);
+
+		foreach ( $heatings as $heating ) {
+			$type_field = $heating . '_erzeugung';
+			$year_field = $heating . '_baujahr';
+
+			if ( in_array( $this->energieausweis->$type_field, $types, true ) && ! empty( $this->energieausweis->$year_field ) && $this->energieausweis->$year_field <= $current_year - $min_age ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
