0a2bbf022070f113883628fe5981de7ff7caa922
Rene Reimann <rene@die-pixler.de>
2020-03-14 20:01:42 +0100

    add docker-compose.yml docker-wp-config backup .htaccess as .htaccess-original reduce .htaccess

---
+++ public/local-config.php
@@ -0,0 +1,15 @@
+<?php
+define( 'WP_ENV', 'development' );
+
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'enon_dev_db' );
+
+define( 'WP_HOME', 'http://enon.dev' );
+define( 'WP_SITEURL', 'http://enon.dev' );
+define( 'RELOCATE', true );
+
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+
+@ini_set( 'display_errors', 1 );
