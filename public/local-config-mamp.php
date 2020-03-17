f0da7a1e937802e7f5f82c491e137e1f5d0ad0cf
Sven Wagener <sven@awesome.ug>
2020-03-17 15:51:46 +0100

    Local mamp config

---
+++ public/local-config-mamp.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'DB_NAME', 'enon' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost' );
+define( 'WP_HOME', 'https://enon.test' );
+define( 'WP_ENV', 'development' );
+define( 'WP_SENTRY_ENV', 'development' );
