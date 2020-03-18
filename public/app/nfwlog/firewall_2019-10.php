e13423372fd30f21e5c42223a053529546c4af1b
Sven Wagener <sven@awesome.ug>
2020-03-18 08:17:51 +0100

    Removed ninja firewall log directory from git

--- public/app/nfwlog/firewall_2019-10.php
+++
@@ -1,3 +0,0 @@
-<?php exit; ?>
-[1571144159] [0] [energieausweis-online-erstellen.test] [#7656789] [0] [6] [192.168.50.1] [403] [GET] [/core/wp-admin/plugins.php] [Plugin deactivated by svenwagener] [hex:4e616d653a2077702d726f636b65742f77702d726f636b65742e706870]
-[1571144358] [0] [energieausweis-online-erstellen.test] [#1986011] [0] [6] [192.168.50.1] [403] [GET] [/core/wp-admin/plugins.php] [Plugin deactivated by svenwagener] [hex:4e616d653a20636f6f6b69652d6c61772d696e666f2f636f6f6b69652d6c61772d696e666f2e706870]
