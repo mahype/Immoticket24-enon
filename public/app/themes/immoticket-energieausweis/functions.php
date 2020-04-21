af90f4134789428c6618c1937fb65a60da5c9ef5
Sven Wagener <sven@awesome.ug>
2020-04-21 16:34:32 +0200

    Added trigger for change on payment method

--- public/app/themes/immoticket-energieausweis/functions.php
+++ public/app/themes/immoticket-energieausweis/functions.php
@@ -227,7 +227,8 @@ function immoticketenergieausweis_payment_icons() {
       check_active();
 
       $( 'input[name="payment-mode"]' ).on( 'change', function( e ) {
-        check_active();
+	     $(document).trigger('wpenon.update_payment_method', [$active_tab_content.index()]);
+         check_active();
       });
 
       $( document ).on( 'click', '.immoticket24-payment-buttons button', function( e ) {
