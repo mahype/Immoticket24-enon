fd5339a32f1a00bc82107f13e81aa2f5a703d334
Sven Wagener <sven@awesome.ug>
2020-04-28 15:07:49 +0200

    Plain image link for tuev #103

--- public/app/plugins/enon/src/Models/Badges/Tuev_Badge.php
+++ public/app/plugins/enon/src/Models/Badges/Tuev_Badge.php
@@ -31,68 +31,9 @@ class Tuev_Badge extends Badge_Component {
 		?>
 		<div id="tuev-wrap">
 			<div id="tuev-badge">
-				<img src="<?php echo plugin_dir_url( dirname( dirname( __DIR__ ) ) ) . 'src/Assets/Img/Badges/tuev-saarland-logo.png'; ?>" />
-			</div>
-			<div id="ekomi-rating">
-				<?php
-				// phpcs:ignore
-				echo $ekomi->get_rating_stars();
-				?>
-				<div class="stars-description">
-					<strong>
-						4.8
-					</strong>/5
-				</div>
-			</div>
-			<div id="tuev-widget">
 				<a href="http://tuev-saar.de/SC44176"><img src="<?php echo plugin_dir_url( dirname( dirname( __DIR__ ) ) ) . 'src/Assets/Img/Badges/tuev-geprueft.png'; ?>" /></a>
 			</div>
 		</div>
-		<?php
-
-		$content = ob_get_clean();
-
-		return $content;
-	}
-
-	public function js() : string {
-		ob_start();
-		?>
-		<script type="text/javascript">
-			jQuery( document ).ready( function( $ ) {
-				$('#tuev-wrap').hover( function () {
-					if ( document.documentElement.clientWidth < 992 ) {
-						return;
-					}
-					$('#tuev-widget').fadeIn(50);
-				}, function () {
-					if ( document.documentElement.clientWidth < 992 ) {
-						return;
-					}
-					$('#tuev-widget').fadeOut(50);
-				});
-			});
-
-			(function (w) {
-				w['_ekomiWidgetsServerUrl'] = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//widgets.ekomi.com';
-				w['_customerId'] = 81266;
-				w['_ekomiDraftMode'] = true;
-				w['_language'] = 'de';
-
-				if(typeof(w['_ekomiWidgetTokens']) !== 'undefined'){
-					w['_ekomiWidgetTokens'][w['_ekomiWidgetTokens'].length] = 'sf812665e4a9b6aad5b9';
-				} else {
-					w['_ekomiWidgetTokens'] = new Array('sf812665e4a9b6aad5b9');
-				}
-
-				if(typeof(ekomiWidgetJs) == 'undefined') {
-					ekomiWidgetJs = true;
-
-					var scr = document.createElement('script');scr.src = 'https://sw-assets.ekomiapps.de/static_resources/widget.js';
-					var head = document.getElementsByTagName('head')[0];head.appendChild(scr);
-				}
-			})(window);
-		</script>
 		<?php
 
 		$content = ob_get_clean();
@@ -106,60 +47,13 @@ class Tuev_Badge extends Badge_Component {
 		<style>
 			.trust .badges #tuev-wrap {
 				padding-left: 8px;
-				background-color: #fafafa;
-				border: 1px solid #dddddd;
-				border-radius: 8px;
 			}
-			.trust .badges #tuev-badge,
-			.trust .badges #tuev-rating {
+			.trust .badges #tuev-badge {
 				display: block;
 				float: left;
 			}
 			.trust .badges #tuev-badge img {
-				width: 54px;
-				margin-top: 8px;
-			}
-			.trust .badges #tuev-rating {
-				margin-top: 15px;
-				padding: 5px;
-				text-align: center;
-			}
-			.trust .badges #tuev-widget {
-				position: absolute;
-				top: 93px;
-				left: -135px;
-				z-index: 10000;
-				display: none;
-				width: 290px;
-				padding: 20px;
-				background: none repeat scroll 0 0 #ffffff;
-				border: 1px solid #aaaaaa;
-			}
-			.trust .badges #tuev-widget:before {
-				content: "";
-				border-bottom: 12px solid #ffffff;
-				border-right: 12px solid transparent;
-				border-left: 12px solid transparent;
-				position: absolute;
-				top: -12px;
-				right: 10%;
-				margin-left: -12px;
-				z-index: 9999999999999999999;
-			}
-			.trust .badges #tuev-widget:after {
-				content: "";
-				border-bottom: 12px solid #aaaaaa;
-				border-right: 12px solid transparent;
-				border-left: 12px solid transparent;
-				position: absolute;
-				top: -13px;
-				right: 10%;
-				margin-left: -12px;
-				z-index: -10000;
-			}
-			.trust .badges #tuev-widget img {
-				display: block;
-				margin: 0 auto;
+				width: 180px;
 			}
 		</style>
 		<?php
