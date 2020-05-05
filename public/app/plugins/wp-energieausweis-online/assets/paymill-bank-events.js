ced72e4f95a3a57bbd2d99ad3859680cde753a18
Rene Reimann <rene@die-pixler.de>
2020-05-05 07:15:14 +0200

    #117 - doo pr requests

--- public/app/plugins/wp-energieausweis-online/assets/paymill-bank-events.js
+++ public/app/plugins/wp-energieausweis-online/assets/paymill-bank-events.js
@@ -5,7 +5,7 @@
  * Include by filter edd_after_cc_fields @ public/app/plugins/wp-energieausweis-online/inc/functions.php
  */
 var format_iban = function(){
-	var wpenon_sepa_fields = document.querySelector('#wpenon_sepa_fields');
+	var wpenon_sepa_fields = document.getElementById('wpenon_sepa_fields');
 
 	var className = {
 		danger: 'label-danger',
@@ -18,24 +18,30 @@ var format_iban = function(){
 	};
 
 	if(wpenon_sepa_fields) {
-		var sepaInputNode = wpenon_sepa_fields.querySelector( '#sepa_account_id' );
+		var sepaInputNode = document.getElementById( 'sepa_account_id' );
 
 		if ( sepaInputNode ) {
+			sepaInputNode.addEventListener( 'input', function() {
+				this.value = this.value.toUpperCase().replace( /[^\dA-Z]/g, '' ).replace( /(.{4})/g, '$1 ' ).trim();
+			});
+
 			sepaInputNode.addEventListener( 'focusout', function() {
 				var successNode = sepaInputNode.parentNode.querySelector( '.is-done-checkmark.label' );
-				var iban = this.value;
 
-				var ibanRegexp = /^DE[a-zA-Z0-9]{2}\s?([0-9]{4}\s?){4}([0-9]{2})\s?/g;
-
-				if ( ibanRegexp.exec( iban ) ) {
-					this.value = iban.replace( /[^\dA-Z]/g, '' ).replace( /(.{4})/g, '$1 ' ).trim();
-					successNode.classList.add(className.success);
-					successNode.classList.remove(className.danger);
-					successNode.innerText = labelContent.success;
-				}else{
+				if(successNode){
 					successNode.classList.remove(className.success);
 					successNode.classList.add(className.danger);
 					successNode.innerText = labelContent.danger;
+				}
+
+				var iban = this.value;
+				var ibanRegexp = /(^[A-Z]{2}\d{2})\s?((\d{4}\s?){4}\s?)(\d{2})$/g;
+				var result = ibanRegexp.exec( iban );
+
+				if ( result ) {
+					successNode.classList.add(className.success);
+					successNode.classList.remove(className.danger);
+					successNode.innerText = labelContent.success;
 				}
 
 			} );
