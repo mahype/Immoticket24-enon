c97685dcd63eaa0c3068ae6f96aae5dffc21fca9
Rene Reimann <rene@die-pixler.de>
2020-05-16 01:06:04 +0200

    enon-#91 Bildgrößen Upload Check - add worker to attache used post_id to thumb metadate

--- public/app/plugins/wpenon_image_cleanup/wpenon_image_cleanup.php
+++ public/app/plugins/wpenon_image_cleanup/wpenon_image_cleanup.php
@@ -11,13 +11,18 @@
  */
 
 add_action('init', function (){
+	$chunk_size = 500;
+
 	$is_runing = !empty($_GET['wpenon_image_cleanup_progress']) ? true : false;
 
 	$item_count = (int) !empty($_GET['wpenon_image_cleanup_items']) ? $_GET['wpenon_image_cleanup_items'] : get_posts_count();
 
-	$cunks = (int) !empty($_GET['wpenon_image_cleanup_chunks']) ? $_GET['wpenon_image_cleanup_chunks'] : get_cunk_size($item_count);
+	$cunks = (int) !empty($_GET['wpenon_image_cleanup_chunks']) ? $_GET['wpenon_image_cleanup_chunks'] : get_cunk_size($item_count, $chunk_size);
+
+	$total = (int) !empty($_GET['wpenon_image_cleanup_total']) ? $_GET['wpenon_image_cleanup_total'] : $cunks;
 
 	if($is_runing){
+		set_marker_on_post_thumbnail($chunk_size);
 		sleep(20);
 	}
 
@@ -28,8 +33,9 @@ add_action('init', function (){
 	}
 
 	$query_args = [
-		'wpenon_image_cleanup_progress' => $is_runing,
+		'wpenon_image_cleanup_progress' => true,
 		'wpenon_image_cleanup_items' => $item_count,
+		'wpenon_image_cleanup_total' => $total,
 		'wpenon_image_cleanup_chunks' => $next,
 	];
 
@@ -38,6 +44,8 @@ add_action('init', function (){
 	});
 
 	$query = build_query($query_args);
+	$progress = round($next*100/$total);
+	header( "wpenon-image-cleanup-progress: " . $progress);
 	wp_redirect( home_url() . '?' . $query );
 	exit();
 });
@@ -48,7 +56,20 @@ function get_posts_count(){
 	return $wpdb->get_results("SELECT count(*) FROM wpit24_posts WHERE wpit24_posts.post_type = 'download';", ARRAY_A)[0]['count(*)'];
 }
 
-function get_cunk_size ($item_count){
-	$chunk_size = 500;
+function get_cunk_size ($item_count, $chunk_size){
 	return round($item_count / $chunk_size, 0);
+}
+
+function set_marker_on_post_thumbnail($chunk_size){
+	$posts = get_posts([
+		                   'post_type' => 'download',
+		                   'numberposts' => $chunk_size
+	                   ]);
+
+	foreach ($posts as $post){
+		$imageId = get_post_meta( $post->ID, '_thumbnail_id', true );
+		update_post_meta($imageId, '_wp_attached_post', $post->ID);
+	}
+
+	return true;
 }
