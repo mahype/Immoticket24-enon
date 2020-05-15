eddf87375a15e69aafbb83e88f2e1edb619df37e
Rene Reimann <rene@die-pixler.de>
2020-05-16 00:31:56 +0200

    enon-#91 Bildgrößen Upload Check - add cleanup worker

---
+++ public/app/plugins/wpenon_image_cleanup/wpenon_image_cleanup.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Plugin Name: wpenon Image Cleanup
+ * Plugin URI:
+ * Description: Aufräum Plugin für enon - macht dinge gut und sauber
+ * Version: 1.0.2
+ * License: Private
+ *
+ * @package WPENON
+ * @version 1.0.2
+ */
+
+add_action('init', function (){
+	$is_runing = !empty($_GET['wpenon_image_cleanup_progress']) ? true : false;
+
+	$item_count = (int) !empty($_GET['wpenon_image_cleanup_items']) ? $_GET['wpenon_image_cleanup_items'] : get_posts_count();
+
+	$cunks = (int) !empty($_GET['wpenon_image_cleanup_chunks']) ? $_GET['wpenon_image_cleanup_chunks'] : get_cunk_size($item_count);
+
+	if($is_runing){
+		sleep(20);
+	}
+
+	$next = $cunks-1;
+
+	if($next === 0){
+		wp_die('wpenon_image_cleanup - fertig mit aufgeräumen');
+	}
+
+	$query_args = [
+		'wpenon_image_cleanup_progress' => $is_runing,
+		'wpenon_image_cleanup_items' => $item_count,
+		'wpenon_image_cleanup_chunks' => $next,
+	];
+
+	add_filter('x_redirect_by', function (){
+		return 'wpenon_image_cleanup';
+	});
+
+	$query = build_query($query_args);
+	wp_redirect( home_url() . '?' . $query );
+	exit();
+});
+
+
+function get_posts_count(){
+	global $wpdb;
+	return $wpdb->get_results("SELECT count(*) FROM wpit24_posts WHERE wpit24_posts.post_type = 'download';", ARRAY_A)[0]['count(*)'];
+}
+
+function get_cunk_size ($item_count){
+	$chunk_size = 500;
+	return round($item_count / $chunk_size, 0);
+}
