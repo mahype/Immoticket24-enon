27bb696f688f60c475bbb4d3dadedb911946973d
Sven Wagener <sven@awesome.ug>
2020-06-23 15:55:01 +0200

    Checking option also for null value #172

--- public/app/plugins/enon/src/Assets/Dist/bundle.js
+++ public/app/plugins/enon/src/Assets/Dist/bundle.js
@@ -147,7 +147,7 @@ class Popup_Premiumbewertung {
 	option_exists() {
 		var option = document.getElementById('edd_custom_fee_premium_bewertung' );
 
-		if ( option !== undefined ){
+		if ( option !== undefined && option !== null ){
 			return true;
 		} else{
 			return false;
