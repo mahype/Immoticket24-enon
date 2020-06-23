085cf9f03b1c4b6ffeb2dd757e527ae66f12cb21
Sven Wagener <sven@awesome.ug>
2020-06-23 15:56:52 +0200

    Formatted code #172

--- public/app/plugins/enon/src/Assets/Dist/bundle.js
+++ public/app/plugins/enon/src/Assets/Dist/bundle.js
@@ -147,9 +147,9 @@ class Popup_Premiumbewertung {
 	option_exists() {
 		var option = document.getElementById('edd_custom_fee_premium_bewertung' );
 
-		if ( option !== undefined && option !== null ){
+		if ( option !== undefined && option !== null ) {
 			return true;
-		} else{
+		} else {
 			return false;
 		}
 	}
