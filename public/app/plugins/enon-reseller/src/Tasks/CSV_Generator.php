12abe6245bfb6b91f82d2f1d7b00e6ccc8d8625a
Sven Wagener <sven@awesome.ug>
2020-07-24 12:09:45 +0200

    Changed charset #169

--- public/app/plugins/enon-reseller/src/Tasks/CSV_Generator.php
+++ public/app/plugins/enon-reseller/src/Tasks/CSV_Generator.php
@@ -226,7 +226,7 @@ class CSV_Generator implements Task, Actions {
 				fputcsv( $f, $line, ';' );
 			}
 			fseek( $f, 0 );
-			header( 'Content-Type: application/csv; charset=utf-8' );
+			header( 'Content-Type: application/csv; charset=iso-8859-1' );
 			header( 'Content-Disposition: attachment; filename="' . $filename . '";' );
 			fpassthru( $f );
 
