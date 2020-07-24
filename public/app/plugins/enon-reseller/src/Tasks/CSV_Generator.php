38cc677ebdfae6a211b2098cb19defa410f60160
Sven Wagener <sven@awesome.ug>
2020-07-24 12:44:49 +0200

    Changed charset #169

--- public/app/plugins/enon-reseller/src/Tasks/CSV_Generator.php
+++ public/app/plugins/enon-reseller/src/Tasks/CSV_Generator.php
@@ -223,7 +223,7 @@ class CSV_Generator implements Task, Actions {
 			$f = fopen( 'php://memory', 'w' );
 
 			foreach ( $result as $line ) {
-				$line = mb_convert_encoding( $line,"Windows-1252", "UTF-8" );
+				$line = iconv(  "Windows-1252", "UTF-8", $line );
 				fputcsv( $f, $line, ';' );
 			}
 			fseek( $f, 0 );
