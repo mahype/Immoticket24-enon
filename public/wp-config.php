4e3c832ecb6a22a20e865c9e7efd4a80d971b428
Sven Wagener <sven@awesome.ug>
2020-08-14 12:09:54 +0200

    Added WP debug logs #212

--- public/wp-config.php
+++ public/wp-config.php
@@ -132,7 +132,8 @@ switch ( WP_ENV ) {
 		break;
 	case 'production':
 	default:
-		define( 'WP_DEBUG', false);
+		define( 'WP_DEBUG', true);
+		define( 'WP_DEBUG_DISPLAY', false );
 		define( 'WP_DEBUG_LOG', dirname( dirname( __FILE__ ) ) . '/debug.log' );
 		define( 'WP_DEBUG_DISPLAY', false );
 		define( 'DISALLOW_FILE_EDIT', true );
