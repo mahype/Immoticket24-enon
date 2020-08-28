bd756f5b1d575479708525e192177843236eb311
Sven Wagener <sven@awesome.ug>
2020-08-28 13:45:11 +0200

    Added Laravel Valet Driver

---
+++ LocalValetDriver.php
@@ -0,0 +1,55 @@
+<?php
+
+class LocalValetDriver extends LaravelValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return true;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        $staticFilePath = $sitePath.'/public'.$uri;
+
+        if ($this->isActualFile($staticFilePath)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if (strpos($uri, '/core') !== false) {
+            return is_dir( $sitePath.'/public/'.$uri )
+                            ? $sitePath.'/public/'.$uri.'/index.php'
+                            : $sitePath.'/public/'.$uri;
+        }
+
+        return $sitePath.'/public/index.php';
+    }
+}
