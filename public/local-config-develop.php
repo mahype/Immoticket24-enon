4c28425274f37a0e1cf87770fb84f37e30fcda4f
Sven Wagener <sven@awesome.ug>
2020-12-08 13:14:56 +0100

    Added develop stage

---
+++ public/local-config-develop.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'DB_NAME', 'd034923b' );
+define( 'DB_USER', 'd034923b' );
+define( 'DB_PASSWORD', 'qma2XhAnd6YtdAVR' );
+define( 'DB_HOST', 'localhost' );
+define( 'WP_HOME', 'https://develop.energieausweis-online-erstellen.de/' );
+define( 'WP_ENV', 'development' );
+define( 'WP_SENTRY_ENV', 'staging' );
