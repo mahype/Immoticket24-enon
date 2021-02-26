9a2a631d14d9986581181adab593e75bf475f273
Sven Wagener <sven@awesome.ug>
2021-02-26 08:14:57 +0100

    Added config

---
+++ public/local-config-2021.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'DB_NAME', 'd0358585' );
+define( 'DB_USER', 'd0358585' );
+define( 'DB_PASSWORD', 'PFxoqbQ52dG6aPvZ' );
+define( 'DB_HOST', 'localhost' );
+define( 'WP_HOME', 'https://2021.energieausweis-online-erstellen.de/' );
+define( 'WP_ENV', 'development' );
