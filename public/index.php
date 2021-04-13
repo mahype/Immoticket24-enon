55be91c671f350f21d357693a290955a09e58f4a
Sven Wagener <sven@awesome.ug>
2021-04-13 08:07:14 +0200

    Ini set samesite none for cookies

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ $cookie_params = session_get_cookie_params();
 $cookie_params['samesite'] = 'None';
 session_set_cookie_params( $cookie_params );
 
+ini_set('session.cookie_samesite', 'None');
+
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
 
