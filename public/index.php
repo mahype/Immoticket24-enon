d5286b08090de478079606161d80ee255f4ded0e
Sven Wagener <sven@awesome.ug>
2021-04-13 08:17:28 +0200

    Added secure param for session cookies

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
 
 $cookie_params = session_get_cookie_params();
 $cookie_params['samesite'] = 'None';
+$cookie_params['secure'] = true;
 session_set_cookie_params( $cookie_params );
-
-ini_set('session.cookie_samesite', 'None');
 
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
