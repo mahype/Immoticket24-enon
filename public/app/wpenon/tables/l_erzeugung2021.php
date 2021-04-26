8d6ae9e4826f293410383ce78f90ae8ce710531d
Sven Wagener <sven@awesome.ug>
2021-04-26 17:22:45 +0200

    Added tables for 2021

---
+++ public/app/wpenon/tables/l_erzeugung2021.php
@@ -0,0 +1,95 @@
+<?php
+
+return array(
+  'title'         => __( 'Lüftungsanlagen 2021', 'wpenon' ),
+  'description'   => __( 'Diese Tabelle enthält die standardisierten Heizwärmegewinne und Hilfsenergiewerte für diverse Lüftungsanlagen.', 'wpenon' ),
+  'asterisks'     => array(
+    'HWG'             => __( 'Heizwärmegewinne', 'wpenon' ),
+    'HE'              => __( 'Hilfsenergiebedarf', 'wpenon' ),
+  ),
+  'primary_field' => 'bezeichnung',
+  'search_field'  => 'bezeichnung',
+  'search_before' => true,
+  'fields'        => array(
+    'bezeichnung'   => array(
+      'title'         => __( 'Bezeichnung (intern)', 'bieaw' ),
+      'type'          => 'VARCHAR(100)',
+    ),
+    'name'          => array(
+      'title'         => __( 'Anlage', 'bieaw' ),
+      'type'          => 'VARCHAR(100)',
+    ),
+    'hwg150_1989' => array(
+      'title'       => __( 'HWG<sup>1</sup> 150m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg500_1989' => array(
+      'title'       => __( 'HWG<sup>1</sup> 500m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg2500_1989'=> array(
+      'title'       => __( 'HWG<sup>1</sup> 2500m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he150_1989'  => array(
+      'title'       => __( 'HE<sup>2</sup> 150m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he500_1989'  => array(
+      'title'       => __( 'HE<sup>2</sup> 500m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he2500_1989' => array(
+      'title'       => __( 'HE<sup>2</sup> 2500m&sup2; (bis 1989)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg150_1994' => array(
+      'title'       => __( 'HWG<sup>1</sup> 150m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg500_1994' => array(
+      'title'       => __( 'HWG<sup>1</sup> 500m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg2500_1994'=> array(
+      'title'       => __( 'HWG<sup>1</sup> 2500m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he150_1994'  => array(
+      'title'       => __( 'HE<sup>2</sup> 150m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he500_1994'  => array(
+      'title'       => __( 'HE<sup>2</sup> 500m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he2500_1994' => array(
+      'title'       => __( 'HE<sup>2</sup> 2500m&sup2; (bis 1994)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg150_1995' => array(
+      'title'       => __( 'HWG<sup>1</sup> 150m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg500_1995' => array(
+      'title'       => __( 'HWG<sup>1</sup> 500m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'hwg2500_1995'=> array(
+      'title'       => __( 'HWG<sup>1</sup> 2500m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he150_1995'  => array(
+      'title'       => __( 'HE<sup>2</sup> 150m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he500_1995'  => array(
+      'title'       => __( 'HE<sup>2</sup> 500m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+    'he2500_1995' => array(
+      'title'       => __( 'HE<sup>2</sup> 2500m&sup2; (ab 1995)', 'bieaw' ),
+      'type'        => 'FLOAT',
+    ),
+  ),
+);
---
+++ public/app/wpenon/tables/uwerte2021.php
@@ -0,0 +1,61 @@
+<?php
+
+return array(
+	'title'         => __( 'U-Werte 2021', 'wpenon' ),
+	'description'   => __( 'Diese Tabelle enthält die standardisierten Wärmedurchgangskoeffizienten (U-Werte) verschiedener Bauteile, eingeteilt in Zeiträumen von Baujahren.', 'wpenon' ),
+	'asterisks'     => array(),
+	'primary_field' => 'bezeichnung',
+	'search_field'  => 'bezeichnung',
+	'search_before' => true,
+	'rebuild_on_import' => true,
+	'fields'        => array(
+		'bezeichnung'   => array(
+			'title'         => __( 'Bezeichnung (intern)', 'wpenon' ),
+			'type'          => 'VARCHAR(100)',
+		),
+		'name'          => array(
+			'title'         => __( 'Bauteil', 'wpenon' ),
+			'type'          => 'VARCHAR(100)',
+		),
+		'bis1918'     => array(
+			'title'       => __( 'Bis 1918', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1948'     => array(
+			'title'       => __( '1919-1948', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1957'     => array(
+			'title'       => __( '1949-1957', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1968'     => array(
+			'title'       => __( '1958-1968', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1978'     => array(
+			'title'       => __( '1969-1978', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1983'     => array(
+			'title'       => __( '1979-1983', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis1994'     => array(
+			'title'       => __( '1984-1994', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis2001'      => array(
+			'title'       => __( '1995-2001', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'bis2006'      => array(
+			'title'       => __( '2002-2006', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+		'ab2007'      => array(
+			'title'       => __( 'Ab 2007', 'wpenon' ),
+			'type'        => 'FLOAT',
+		),
+	),
+);
