5f6d5b927ec263ac994ec2bede73a98f60a8d9a0
Sven Wagener <sven@awesome.ug>
2021-04-28 17:05:02 +0200

    Removed GEG20 check

--- public/app/wpenon/enev2021-01/schema/vw-basisdaten.php
+++ public/app/wpenon/enev2021-01/schema/vw-basisdaten.php
@@ -18,22 +18,6 @@ $basisdaten = array(
 					),
 					'required'    => true,
                 ),
-                'geg20_approval' => array(
-                    'type'  => 'hidden',
-                    'title' => __( 'Wurde für das Bauvorhaben eine Genehmigung beantragt?', 'wpenon' ),          
-                ),
-                'geg20_approval_date' => array(
-                    'type'  => 'hidden',
-                    'title' => __( 'Wann wurde die Genehmigung beantragt?', 'wpenon' ),          
-                ),
-                'geg20_building_measure_date' => array(
-                    'type'  => 'hidden',
-                    'title' => __( 'Wann wurde mit der Baumaßnahme begonnen?', 'wpenon' ),          
-                ),
-                'geg20_creation_denied' => array(
-                    'type'  => 'hidden',
-                    'title' => __( 'Kauf des Ausweises verweigert?', 'wpenon' ),          
-                ),
 			),
         ),
 		'gebaeude'       => array(
