1b55379498d44ec587f209a1eff07673acc1ae2c
Sven Wagener <sven@awesome.ug>
2021-07-19 14:49:26 +0200

    Neuer Standard für nächsten Tag gesetzt #339

--- public/app/plugins/enon/src/Enon/Standards_Config.php
+++ public/app/plugins/enon/src/Enon/Standards_Config.php
@@ -72,9 +72,9 @@ class Standards_Config extends Config {
 				'start_date' => '2021-06-30',
 			),
 			'enev2021-05' => array(
-				'name'       => __( 'GEG 2021 (ab 19.07.2021)', 'wpenon' ),
+				'name'       => __( 'GEG 2021 (ab 20.07.2021)', 'wpenon' ),
 				'date'       => '2020-08-08',
-				'start_date' => '2021-07-19',
+				'start_date' => '2021-07-20',
 			),
 		);
 	}
