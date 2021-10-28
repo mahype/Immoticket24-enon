afb143ec171bd3c1c93bcfb8e980a6bca58bf5a7
Sven Wagener <sven@awesome.ug>
2021-10-28 10:31:53 +0200

    Umbau Fileupload aufgrund von Fehler nach Update auf Staging

--- public/app/plugins/wp-energieausweis-online/inc/functions.php
+++ public/app/plugins/wp-energieausweis-online/inc/functions.php
@@ -1,12 +1,13 @@
 <?php
 
-
-
 /**
  * @package WPENON
  * @version 1.0.2
  * @author Felix Arntz <felix-arntz@leaves-webdesign.com>
  */
+
+
+ require dirname( __FILE__ ) . '/image-upload.php';
 
 function wpenon_get_energieausweis( $energieausweis = null ) {
 	return \WPENON\Model\EnergieausweisManager::getEnergieausweis( $energieausweis );
@@ -305,123 +306,3 @@ if ( ! function_exists( 'edd_sanitize_price' ) ) {
 add_action('edd_after_cc_fields', function(){
 	echo '<script type="text/javascript" src="' . plugin_dir_url(__DIR__) . '/assets/paymill-bank-events.js"></script>';
 });
-
-
-function wpenon_register_rest_api_endpoint_image_upload() {
-	register_rest_route( 'ec', '/image_upload/', array(
-		'methods' => 'POST',
-		'callback' => 'wpenon_image_upload',
-		'permission_callback' => '__return_true'
-	) );
-}
-
-add_action( 'rest_api_init', 'wpenon_register_rest_api_endpoint_image_upload' );
-
-function wpenon_register_rest_api_endpoint_image_delete() {
-	register_rest_route( 'ec', '/image_delete/', array(
-		'methods' => 'POST',
-		'callback' => 'wpenon_image_delete',
-		'permission_callback' => '__return_true'
-	) );
-}
-
-add_action( 'rest_api_init', 'wpenon_register_rest_api_endpoint_image_delete' );
-
-function wpenon_image_delete( \WP_REST_Request $request )
-{
-	$ecId  = $request->get_param( 'ecId' );
-	$field = $request->get_param( 'field' );
-
-	$postMetaUrl      = $field. '_image';
-	$postMetaFileName = $field. '_image_file';
-
-	$fileName = get_post_meta( $ecId, $postMetaFileName, true );
-	@unlink( $fileName );
-
-	delete_post_meta( $ecId, $postMetaUrl );
-	delete_post_meta( $ecId, $postMetaFileName );
-}
-
-/**
- * AJAX uploads in frontend
- */
-function wpenon_image_upload( \WP_REST_Request $request ) {
-	$ecId  = $request->get_param( 'ecId' );
-	$field = $request->get_param( 'field' );
-	$postMetaName = $field. '_image';
-
-	$file  = $request->get_file_params();
-	$oldFile = get_post_meta( $ecId, $postMetaName, true );
-
-	if( $file['file']['size'] === 0 ) {
-		echo json_encode( ['error' => 'Datei hat keinen Inhalt'] );
-		exit;
-	}
-
-	if( $file['file']['type'] !== 'image/png' &&  $file['file']['type'] !== 'image/jpeg'  ) {		
-		echo json_encode( ['error' => 'Falscher Dateityp'] );
-		exit;
-	}	
-
-	$uploadedFile = wp_handle_upload( $file['file'], [ 'test_form' => FALSE ] );
-	
-	if( ! isset( $uploadedFile['file'] ) ) {		
-		echo json_encode( ['error' => 'Datei konnte nicht verschoben werden'] );
-		exit;
-	}
-	
-	$imageEditor = wp_get_image_editor( $uploadedFile['file'] );	
-	if( is_wp_error( $imageEditor ) ) {		
-		echo json_encode( ['error' => $imageEditor->get_error_message() ] );
-		exit;
-	}
-
-	$imageEditor->maybe_exif_rotate();
-	$setQuality = $imageEditor->set_quality( 35 );
-
-	$size = $imageEditor->get_size();
-	if( $size[0] > 800 || $size[1] > 800 || ( isset( $size['width'] ) && $size['width'] > 800 ) || ( isset( $size['height'] ) && $size['width'] > 800 ) )
-	{
-		$resized = $imageEditor->resize( 800, 800 );	
-	}	
-
-	if( is_wp_error( $resized ) ) {
-		echo json_encode( ['error' => $resized->get_error_message() ] );
-		exit;
-	}
-
-    $resizedFile = $imageEditor->save();
-
-	if( is_wp_error( $resizedFile ) ) {
-		echo json_encode( ['error' => $resizedFile->get_error_message() ] );
-		exit;
-	}
-
-	$upload_dir = wp_upload_dir();
-
-	$randInt = random_int ( PHP_INT_MIN, PHP_INT_MAX );
-	$prefix = $field . '_' . md5( $randInt );
-	$suffix = $file['file']['type'] === 'image/jpeg' ? 'jpg': 'png' ;
-		
-	$filename = $prefix . '.' . $suffix;
-		
-	rename( $resizedFile['path'], trailingslashit( $upload_dir['path'] ) . $filename );	
-	unlink( $uploadedFile['file']);
-	
-	$fileUrl =  trailingslashit( $upload_dir['url'] ) . basename( $filename );
-
-	if( 'undefined' == $fileUrl || 'undefined' == trailingslashit( $upload_dir['path'] ) ) {
-		echo json_encode( ['error' => 'Probleme bei Dateiupload, bitte laden Sie eine andere Datei hoch.' ] );
-		exit;
-	}
-
-	update_post_meta( $ecId, $postMetaName . '_file', trailingslashit( $upload_dir['path'] ) . $filename  );
-	update_post_meta( $ecId, $postMetaName, $fileUrl );
-
-	if( ! empty( $oldFile ) && file_exists( $oldFile ) ) {
-		@unlink( $oldFile );
-	}
-
-	echo json_encode( ['url' => $fileUrl ] );
-	exit;
-}
