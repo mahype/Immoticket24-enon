cc894d0ba930002cd808c4e12902412ebb856413
Sven Wagener <sven@awesome.ug>
2021-11-10 15:23:10 +0100

    Post delete forced

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,4 +1,5 @@
 wp search-replace "enon.test" "2021.energiausweis-online-erstellen.de"
 wp search-replace "wp.test" "2021.energieausweis-online-erstellen.de"
 wp search-replace "wp-content/" "app/"
-wp post delete $(wp post list --post_type=page --format=ids)
+
+wp post delete $(wp post list --post_type=page --format=ids) --force
