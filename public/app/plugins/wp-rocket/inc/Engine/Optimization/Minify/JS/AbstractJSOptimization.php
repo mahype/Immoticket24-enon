0c6bd73645b95e981a26d8a9a682c2ecb58d7fd0
Sven Wagener <sven@awesome.ug>
2022-01-07 09:37:40 +0100

    Updated WP Rocket to 3.10.6

--- public/app/plugins/wp-rocket/inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
+++ public/app/plugins/wp-rocket/inc/Engine/Optimization/Minify/JS/AbstractJSOptimization.php
@@ -247,6 +247,10 @@ abstract class AbstractJSOptimization extends AbstractOptimization {
 			'www.vbt.io/ext/vbtforms.js',
 			'cdn.callrail.com',
 			'documentcloud.adobe.com/view-sdk/main.js',
+			'static.cleverpush.com',
+			'js.afterpay.com',
+			'cdn.enable.co.il/licenses/enable-',
+			'hcaptcha.com/1/api.js',
 		];
 
 		$excluded_external = array_merge( $defaults, $this->options->get( 'exclude_js', [] ) );
