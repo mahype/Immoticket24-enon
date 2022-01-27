3ad1e67564783501fa41df9b21bd315d5e39b01e
Sven Wagener <sven@awesome.ug>
2022-01-27 16:15:17 +0100

    Import / Export posts

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wp export --post_type=page --filename_format=pages.xml
+wp export --post_type=post --filename_format=posts.xml
 wp menu export --all --filename=menu.json --allow-root
 wp gf form export --filename=forms.json
--- scripts/import.sh
+++ scripts/import.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wp import pages.xml --authors=skip
+wp import posts.xml --authors=skip
 wp menu import menu.json --allow-root
 wp option update show_on_front 'page'
 wp option update page_on_front 465251
