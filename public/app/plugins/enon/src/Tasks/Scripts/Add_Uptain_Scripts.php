7d4ec213640c621ddfc98023b684b6955fb9411c
Sven Wagener <sven@awesome.ug>
2022-02-07 10:48:52 +0100

    Fixed comments and typo #390

--- public/app/plugins/enon/src/Tasks/Scripts/Add_Uptain_Scripts.php
+++ public/app/plugins/enon/src/Tasks/Scripts/Add_Uptain_Scripts.php
@@ -19,17 +19,15 @@ use Enon\Models\Scripts\Script_Loader;
  *
  * @package awsmug\Enon\Tools
  *
- * @since 2010-09-10
+ * @since 2020-09-10
  */
 class Add_Uptain_Scripts extends Script_Loader {
 	/**
 	 * Funnel page.
 	 *
-	 * @since 2010-09-10
+	 * @since 2020-09-10
 	 */
 	protected function ec_funnel_contacting_allowed() {
-		;
-
 		echo $this->data_tag( [
 			'email'   => $this->ec_mail(),
 			'scv'     => $this->ec_price(),
@@ -42,7 +40,7 @@ class Add_Uptain_Scripts extends Script_Loader {
 	/**
 	 * Success page
 	 * 
-	 * @since 2010-09-16
+	 * @since 2020-09-16
 	 */
 	protected function is_success() {
 		global $edd_receipt_args;
@@ -72,7 +70,7 @@ class Add_Uptain_Scripts extends Script_Loader {
 	/**
 	 * Script files.
 	 * 
-	 * @since 2010-09-10
+	 * @since 2020-09-10
 	 */
 	protected function script_files() : array {
 		return array('https://app.uptain.de/js/uptain.js?x=hnne9BonaNH0mRA0');
