3de254a4a2cc20acc03aca481098f53ea84d5870
Sven Wagener <sven@awesome.ug>
2022-02-23 13:56:22 +0100

    Removed comments and saving field value also on API request #401

--- public/app/plugins/wp-energieausweis-online/templates/admin/schemafields.php
+++ public/app/plugins/wp-energieausweis-online/templates/admin/schemafields.php
@@ -124,17 +124,6 @@
 
                 <?php break; ?>
                 <?php case 'image': ?>
-
-<!-- RAW Data
-<?php 
-global $post;
-print_r( $field_slug );
-print_r( $post ); 
-print_r( $field ); 
-print_r( get_post_meta( $post->ID, $field_slug, false ) );
-print_r( get_post_meta( $post->ID ) );
-?>
--->
             
                 <div id="<?php echo $field_slug; ?>_image">
                   <?php if( ! empty( $field['value'] ) ): ?>
