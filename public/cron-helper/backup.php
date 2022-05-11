594485058f49503bff0d22b5d7ef41d0a6bf228e
Sven Wagener <sven@awesome.ug>
2022-05-11 18:46:33 +0200

    Mail me

---
+++ public/cron-helper/backup.php
@@ -0,0 +1,7 @@
+<?php
+exec("/bin/bash /Backup_Extern/Backup_Script/wordpress_backup_script_bitplex.sh  2>&1", $out, $result);
+echo "Returncode: " .$result ."<br>";
+echo "Ausgabe des Scripts: " ."<br>";
+echo "<pre>"; print_r($out);
+
+mail("sven@awesome.ug", "Backup Test", print_r( $out, true ) );
