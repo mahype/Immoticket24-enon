f4ab18af2c347fb618f25d5f7528bba1eb2218e5
Sven Wagener <sven@awesome.ug>
2022-07-15 11:37:39 +0200

    Added vendor dir #427

---
+++ public/app/themes/jason/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'awsmug/jason',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bf9b4cdad77b2de4e1afd07a47aa3a95b10a15a2',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'awsmug/jason' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bf9b4cdad77b2de4e1afd07a47aa3a95b10a15a2',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
