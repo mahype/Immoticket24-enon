a12939eed61be04e512162160e5bd68d54596e15
Sven Wagener <sven@awesome.ug>
2022-12-07 16:45:33 +0100

    Added sortable column for pending referals #465

--- public/app/plugins/enon/src/Tasks/Setup_AffilateWP.php
+++ public/app/plugins/enon/src/Tasks/Setup_AffilateWP.php
@@ -13,9 +13,6 @@ namespace Enon\Tasks;
 
 use Awsm\WP_Wrapper\Interfaces\Filters;
 use Awsm\WP_Wrapper\Interfaces\Task;
-use Awsm\WP_Wrapper\Tools\Logger;
-
-use Enon\Models\Enon\Prevent_Completion;
 
 /**
  * Class Setup_Edd.
@@ -40,7 +37,9 @@ class Setup_AffilateWP implements Task, Filters {
 	 * @since 1.0.0
 	 */
 	public function add_filters() {
-		// add_filter('affwp_affiliate_table_columns', array( $this, 'add_pending_column' ), 10, 3 );
+	    add_filter('affwp_affiliate_table_columns', array( $this, 'add_pending_column' ), 10, 3 );
+        add_filter('affwp_affiliate_table_pending_referals', array( $this, 'add_pending_column_content' ), 10, 2 );
+        add_filter('affwp_affiliate_table_sortable_columns', array( $this, 'add_sortable_column' ), 10, 1 );
 	}
 
     /**
@@ -55,7 +54,54 @@ class Setup_AffilateWP implements Task, Filters {
      * @since 1.0.0
      */
     public function add_pending_column( $prepared_columns, $columns, $list_table ) {
-        $prepared_columns['pending'] = __( 'Pending', 'affiliate-wp' );
+        $pos = 7;
+        $new_col['pending_referals'] = __( 'Pending', 'affiliate-wp' );
+
+        $prepared_columns = array_merge(
+            array_slice($prepared_columns, 0, $pos),
+            $new_col,
+            array_slice($prepared_columns, $pos)
+        );
+        
         return $prepared_columns;
+    }
+
+    /**
+     * Add the pending referrals column to the sortable columns array.
+     * 
+     * @param array $columns Array of sortable columns.
+     * 
+     * @return array
+     * 
+     * @since 1.0.0
+     */
+    public function add_sortable_column( $columns ) {
+        $pos = 6;
+        
+        $new_col['pending_referals'] = array( 'pending', false );
+
+        $columns = array_merge(
+            array_slice($columns, 0, $pos),
+            $new_col,
+            array_slice($columns, $pos)
+        );
+        
+        return $columns;
+    }
+
+    /**
+     * Add the pending referrals count to the pending column.
+     * 
+     * @param string $value      Column value.
+     * @param object $affiliate  Affiliate object.
+     * 
+     * @return string
+     * 
+     * @since 1.0.0
+     */
+    public function add_pending_column_content( $value, $affiliate ) {
+		$pending_count = affiliate_wp()->referrals->count_by_status( 'pending', $affiliate->affiliate_id );
+		$value = affwp_admin_link( 'referrals', $pending_count, array( 'affiliate_id' => $affiliate->affiliate_id, 'status' => 'pending' ) );
+        return $value;
     }
 }
