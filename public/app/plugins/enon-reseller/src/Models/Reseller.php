069d365525f7ec03101944fba5fb098b2949331b
Sven Wagener <sven@awesome.ug>
2023-03-07 13:01:52 +0100

    Logging Height #486

--- public/app/plugins/enon-reseller/src/Models/Reseller.php
+++ public/app/plugins/enon-reseller/src/Models/Reseller.php
@@ -149,6 +149,7 @@ class Reseller
 		  
 			let send_document_height = function () {
 			  var height = get_wrapper_height();
+			  console.log( \'sending height: \' + height );
 			  parent.postMessage( JSON.stringify( {\'frame_height\': height } ), \'*\' );
 			}
 		  
