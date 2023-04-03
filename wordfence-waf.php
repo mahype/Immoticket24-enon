79fb93f232ec6941a2b0851446ec0ed57b54f7c3
Sven Wagener <sven@awesome.ug>
2023-04-03 13:20:10 +0200

    Added wordfence-waf.php for staging

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -1,7 +1,7 @@
 <?php
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
-if (file_exists(__DIR__.'/../app/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", __DIR__.'/../app/wflogs/');
-	include_once __DIR__.'/../app/plugins/wordfence/waf/bootstrap.php';
+if (file_exists(__DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", __DIR__.'/public/app/wflogs/');
+	include_once __DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php';
 }
