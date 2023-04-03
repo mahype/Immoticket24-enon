2e79212e1256b54431b0218f0488f20dfcf7cb68
Sven Wagener <sven@awesome.ug>
2023-04-03 13:39:26 +0200

    Moved Log dir for WAF

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -2,6 +2,6 @@
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
 if (file_exists(__DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", __DIR__.'/public/app/wflogs/');
+	define("WFWAF_LOG_PATH", __DIR__.'/logs/wflogs/');
 	include_once __DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php';		
 }
