4c25a3f7177d681cdbb71e84c2ff504b38dd867b
Sven Wagener <sven@awesome.ug>
2023-04-03 13:37:28 +0200

    Removed Test of WAF

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -3,12 +3,5 @@
 
 if (file_exists(__DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php')) {
 	define("WFWAF_LOG_PATH", __DIR__.'/public/app/wflogs/');
-	include_once __DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php';
-	$file = fopen(dirname(__FILE__) . '/test.txt', 'w');
-	fwrite($file, 'included');
-	fclose($file);
-} else {
-	$file = fopen(dirname(__FILE__) . '/test.txt', 'w');
-	fwrite($file, 'not included');
-	fclose($file);
+	include_once __DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php';		
 }
