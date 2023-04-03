cc0987e406fc4f54d1da09a5551221f1a9bd2c8f
Sven Wagener <sven@awesome.ug>
2023-04-03 13:34:39 +0200

    Test of WAF

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -4,4 +4,11 @@
 if (file_exists(__DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php')) {
 	define("WFWAF_LOG_PATH", __DIR__.'/public/app/wflogs/');
 	include_once __DIR__.'/public/app/plugins/wordfence/waf/bootstrap.php';
+	$file = fopen(dirname(__FILE__) . '/test.txt', 'w');
+	fwrite($file, 'included');
+	fclose($file);
+} else {
+	$file = fopen(dirname(__FILE__) . '/test.txt', 'w');
+	fwrite($file, 'not included');
+	fclose($file);
 }
