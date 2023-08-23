efd8bc192662f86597f6b1a12cf3cad3cf1d96e7
Sven Wagener <sven@awesome.ug>
2023-08-23 13:17:36 +0200

    Reordered data and added values to bw #521

--- public/app/wpenon/enev2023-01/datenerfassung/templates/Zusatzdatenerfassung-expowand-sendPersonalisiertVW.php
+++ public/app/wpenon/enev2023-01/datenerfassung/templates/Zusatzdatenerfassung-expowand-sendPersonalisiertVW.php
@@ -28,11 +28,10 @@ $data = new DataEnevVW( $energieausweis );
     <n1:Addresse2><?php echo $data->get_certificate_buyer_address_2() ?></n1:Addresse2>
     <n1:Postleitzahl><?php echo $data->get_certificate_buyer_zip() ?></n1:Postleitzahl>
     <n1:Ort><?php echo $data->get_certificate_buyer_city() ?></n1:Ort>
+    <n1:Bundesland><?php echo $data->Bundesland(); ?></n1:Bundesland>
     <n1:Land><?php echo $data->get_certificate_buyer_country() ?></n1:Land>
     <n1:EMail><?php echo $data->get_certificate_buyer_email() ?></n1:EMail>
-    <n1:Telefon><?php echo $data->get_certificate_buyer_phone() ?></n1:Telefon>
-    <n1:Bundesland><?php echo $data->Bundesland(); ?></n1:Bundesland>
-    <n1:Postleitzahl><?php echo $data->PLZ(); ?></n1:Postleitzahl>
+    <n1:Telefon><?php echo $data->get_certificate_buyer_phone() ?></n1:Telefon>        
     <n1:Gebaeudeteil><?php echo $data->Gebauedeteil(); ?></n1:Gebaeudeteil>
     <n1:Baujahr-Gebaeude><?php echo $data->BaujahrGebaeude(); ?></n1:Baujahr-Gebaeude>
     <n1:Altersklasse-Gebaeude><?php echo $data->AltersklasseGebaeude(); ?></n1:Altersklasse-Gebaeude>
