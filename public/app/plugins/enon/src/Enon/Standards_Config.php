4577ff032fe1267159dbc213e49522cd233a9b69
Sven Wagener <sven@awesome.ug>
2023-09-12 16:44:22 +0200

    Added schema #528

--- public/app/plugins/enon/src/Enon/Standards_Config.php
+++ public/app/plugins/enon/src/Enon/Standards_Config.php
@@ -91,6 +91,11 @@ class Standards_Config extends Config {
 				'date'       => '2020-08-08',
 				'start_date' => '2023-07-07',
 			),
+			'enev2023-02' => array(
+				'name'       => __( 'GEG 2021 (ab 12.09.2023)', 'wpenon' ),
+				'date'       => '2020-08-08',
+				'start_date' => '2023-09-12',
+			),
 		);
 	}
 
