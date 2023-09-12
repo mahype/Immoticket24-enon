bde4c028a8b619461fe8dcbbf3ce4b6251044a2f
Sven Wagener <sven@awesome.ug>
2023-09-12 16:50:42 +0200

    Added din18599 scripts for auto deployment #528

---
+++ public/local-config-din18599.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'DB_NAME', 'd03e171f' );
+define( 'DB_USER', 'd03e171f' );
+define( 'DB_PASSWORD', 'LCGAgeLFkqmLJv6ddm8J' );
+define( 'DB_HOST', 'localhost' );
+define( 'WP_HOME', 'https://din18599.energieausweis-online-erstellen.de/' );
+define( 'WP_ENV', 'development' );
