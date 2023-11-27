2535dbf8508aaa2dc692e531f9ec94eb81e913ab
Sven Wagener <sven@awesome.ug>
2023-11-27 11:36:12 +0100

    Konventioneller Kessel angelegt

---
+++ public/app/wpenon/enev2023-02/calculations/Anlagentechnik/Heizungsanlagen/Konventioneller-Kessel.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Enev\Schema202302\Calculations\Anlagentechnik;
+
+use Enev\Schema202302\Calculations\Calculation_Exception;
+use Enev\Schema202302\Calculations\Gebaeude\Gebaeude;
+
+class Konventioneller_Kessel extends Heizungsanlage {
+
+    /**
+     * Pufferspeicher.
+     * 
+     * @var Gebaeude 
+     */
+    protected Gebaeude $gebaeude;
+    /**
+     * Konstruktor.
+     * 
+     * @param string $typ 
+     * @param string $energietraeger 
+     * @param string $auslegungstemperaturen 
+     * @param bool $heizung_im_beheizten_bereich 
+     * @param int $prozentualer_anteil 
+     * @param float $ithrl 
+     * 
+     * @return void 
+     */
+	public function __construct( 
+        Gebaeude $gebaeude,
+        string $typ, 
+        string $energietraeger, 
+        string $auslegungstemperaturen, 
+        bool $heizung_im_beheizten_bereich, 
+        int $prozentualer_anteil = 100, 
+        ) {
+
+		parent::__construct( $typ, $energietraeger, $auslegungstemperaturen, $heizung_im_beheizten_bereich, $prozentualer_anteil );
+        
+        $this->gebaeude = $gebaeude;
+	}
+
+    /**
+     * Bestimmung von $eg0.
+     * 
+     * @return float 
+     * @throws Calculation_Exception 
+     */
+	public function ewg(): float {
+		// $ewg= ($eg-1)*8760/$calculations['ithrl']+1;
+		return ( $this->eg() - 1 ) * 8760 / $this->gebaeude->ith_rl() + 1;
+	}
+
+    /**
+     * 
+     * @return float 
+     */
+	public function eg(): float {
+		// $eg=$eg0*$fbj*$fegt
+		return $this->eg0() * $this->fbj() * $this->fegt();
+	}
+
+    /**
+     * Bestimmung von $eg0.
+     * 
+     * @return float 
+     */
+	public function eg0(): float {
+        //   if "Umlaufwasserheizer" than
+        //      $eg0 = Tab 82 T12, in Anhängigkeit von $Pn und $ßhg
+        //   else
+        //       $eg0 = Tab 77 T12, in Anhängigkeit von $Pn und $ßhg
+		return 0.0;
+	}
+
+	public function ßhg(): float {
+		// $ßhg = $ßhs * $ehs // mittlere Belastung der *Übergabe Heizung; damit für Alle Erzeuger (einschl Fern/Nahwärme)
+        return $this->gebaeude->heizsystem()->pufferspeicher()->ßhs() * $this->gebaeude->heizsystem()->pufferspeicher()->ehs();
+	}
+
+	public function fbj(): float {
+		// if "Umlaufwasserheizer" than
+		// $fbj = Tab 82 T12, in Anhängigkeit von "Umlaufwasserheizer" und $ßhg
+		// else
+		// $fbj = Tab 78 T12, in Anhängigkeit von "Baujahr der Heizung" und $ßhg
+		return 0.0;
+	}
+
+	public function fegt(): float {
+		// if Umlaufwasserheizer &&  "Energieträger = Hackschnitzel" && "Energieträger = Scheitholz" && "Energieträger = Pellet"  than
+		// $fegt = 1.0
+		// if  "Brennwertkessel" &&  "Energieträger = Gas" && "Energieträger = Biogas" && "Energieträger = Flüssiggas"  than
+		// $fegt = Tab.79  in Abhängigkeit  "Vor- und Rücklauftemperatur" und $ßhg und "unbeheizt/beheizt"
+		// if  "Brennwertkessel" &&  "Energieträger = Heizöl" && "Energieträger = Bioöl"  than
+		// $fegt = Tab.80  in Abhängigkeit  "Vor- und Rücklauftemperatur" und $ßhg und "unbeheizt/beheizt"
+		// else
+		// $fegt = Tab. 81 in Abhängigkeit  "Vor- und Rücklauftemperatur" und $ßhg und "unbeheizt/beheizt"
+		return 0.0;
+	}
+}
