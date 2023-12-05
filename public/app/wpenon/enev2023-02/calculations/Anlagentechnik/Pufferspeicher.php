ff2d00db64a5ecff13755f0bc572c48a22016003
Sven Wagener <sven@awesome.ug>
2023-12-05 10:25:19 +0100

    Debuggable Value

--- public/app/wpenon/enev2023-02/calculations/Anlagentechnik/Pufferspeicher.php
+++ public/app/wpenon/enev2023-02/calculations/Anlagentechnik/Pufferspeicher.php
@@ -197,10 +197,12 @@ class Pufferspeicher {
 	 * @throws Calculation_Exception 
 	 */
 	public function ehs(): float {
+		$ehs = 1;
+
 		if( $this->gebaeude->heizsystem()->heizungsanlagen()->waermepumpe_vorhanden()  || $this->gebaeude->heizsystem()->heizungsanlagen()->biomassekessel_vorhanden() ) {
-			return 1 + $this->Qhs() / ( $this->gebaeude->qh() * $this->gebaeude->heizsystem()->uebergabesysteme()->erstes()->ehce() * $this->gebaeude->heizsystem()->ehd_korrektur() );
+			$ehs = 1 + $this->Qhs() / ( $this->gebaeude->qh() * $this->gebaeude->heizsystem()->uebergabesysteme()->erstes()->ehce() * $this->gebaeude->heizsystem()->ehd_korrektur() );
 		}
 
-		return 1;
-	} 
+		return $ehs;
+	}
 }
