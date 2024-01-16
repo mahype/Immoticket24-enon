2ad046d9e89c42c992cfbe2392d2ab9367061990
Sven Wagener <sven@awesome.ug>
2024-01-16 18:44:30 +0100

    Revert "Datenerfassung an das neue Schema angepasst #612"
    This reverts commit f1aa6cf2a995a5f9a3a6667727b0f73d86d40dea.

--- public/app/wpenon/enev2024-01/datenerfassung/templates/Datenerfassung.php
+++ public/app/wpenon/enev2024-01/datenerfassung/templates/Datenerfassung.php
@@ -19,9 +19,10 @@ $art = $energieausweis->mode == 'b' ? 'Energiebedarfsausweis' : 'Energieverbrauc
         <Aussteller_PWD_DIBT><?php echo $pass; ?></Aussteller_PWD_DIBT>
     </Authentifizierung>
     <EnEV-Nachweis>
-        <Ausstellungsdatum><?php echo date('Y-m-d' ); ?></Ausstellungsdatum>
+        <Ausstellungsdatum><?php echo date('d.m.Y' ); ?></Ausstellungsdatum>
         <Bundesland><?php echo $energieausweis->adresse_bundesland; ?></Bundesland>
         <Postleitzahl><?php echo $energieausweis->adresse_plz; ?></Postleitzahl>
+        <Gesetzesgrundlage>GEG-2020</Gesetzesgrundlage>
     </EnEV-Nachweis>
     <Energieausweis-Daten>
         <Gebaeudeart>Wohngebäude</Gebaeudeart>
