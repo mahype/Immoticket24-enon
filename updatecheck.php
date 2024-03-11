af4e70077542c2620aec4f3ddce58165c8499c0a
Frank Neumann-Staude <frank@staude.net>
2024-03-11 09:25:07 +0100

    add updatecheck

---
+++ updatecheck.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * WordPress-Plugin-Status-Update-Check
+ * 
+ * Dieses Skript führt den Befehl `wp plugin status` in einem WordPress-Verzeichnis aus und sendet die Ausgabe an einen Slack-Webhook.
+ * 
+ * @author Frank Neumann-Staude <frank@awesome.ug>
+ * @version 1.0.0
+ * 
+ */
+
+// Pfad zum WordPress-Unterverzeichnis
+$wordpressDir = 'public/app';
+
+// Slack Webhook URL
+$slackWebhookUrl = 'https://hooks.slack.com/services/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';
+
+// Befehl zum Ausführen
+$command = 'wp plugin status';
+
+chdir($wordpressDir);
+$output = [];
+$return_var = 0;
+exec($command, $output, $return_var);
+
+$outputString = implode("\n", $output);
+
+$data = [
+    'text' => "WP Plugin Status:\n" . $outputString,
+];
+
+$ch = curl_init($slackWebhookUrl);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json'
+]);
+
+$result = curl_exec($ch);
+curl_close($ch);
