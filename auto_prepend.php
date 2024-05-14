c9eb15c0f21bef26e65e714b0140589c76eac388
Sven Wagener <sven@awesome.ug>
2024-05-14 10:52:52 +0200

    Auto Prepend Reihenfolge

--- auto_prepend.php
+++ auto_prepend.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * WordFence Security plugin.
- */
-require dirname(__FILE__) . '/wordfence-waf.php';
-
-
-/**
  * Auto prepended file to handle errors.
  */
 require dirname(__FILE__) . '/error_handling.php';
+
+/**
+ * WordFence Security plugin.
+ */
+require dirname(__FILE__) . '/wordfence-waf.php';
