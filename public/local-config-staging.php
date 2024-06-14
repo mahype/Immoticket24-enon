d91636f103b6eafe2f3041524ea0fcfe64e57395
Sven Wagener <sven@awesome.ug>
2024-06-14 11:04:01 +0200

    Staging Domain geändert

--- public/local-config-staging.php
+++ public/local-config-staging.php
@@ -4,6 +4,6 @@ define( 'DB_NAME', 'd03116bd' );
 define( 'DB_USER', 'd03116bd' );
 define( 'DB_PASSWORD', 'Qs7wBEVhgqf9nZdn' );
 define( 'DB_HOST', 'localhost' );
-define( 'WP_HOME', 'https://staging.energieausweis-online-erstellen.de/' );
+define( 'WP_HOME', 'https://staging.energieausweis.de/' );
 define( 'WP_ENV', 'development' );
 define( 'WP_SENTRY_ENV', 'staging' );
